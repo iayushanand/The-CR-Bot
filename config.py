89447017251f35c1bb8e0daa58e9856ca18ac4b7
Ayu py <77532758+ayush-py-dev@users.noreply.github.com>
2023-02-10 21:37:09 +0530

    example config

---
+++ config.py
@@ -0,0 +1,8 @@
+# example config file
+
+port=06969
+password="I-make-good-bot"
+token="MTA3MjA1Nzg5NjM1NzA3NzAxMg.GTy_7-.TMQWNtS3ZUpvTWTPzqQk2B-5DgMycJcqDjqZr8" # <--- totally legit bot token. If says INVALID TOKEN PASSED, then you're gay :)
+mod=1072453600032342036
+ip="127.0.0.1"
+log=1072611320979017748
