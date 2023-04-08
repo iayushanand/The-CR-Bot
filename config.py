bb8a8df3252bb70db1e0e3cd0f172f1c2ed68df9
Ayu Itz <77532758+ayush-py-dev@users.noreply.github.com>
2023-04-08 17:06:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ token="MTA3MjA1Nzg5NjM1NzA3NzAxMg.GFESyM.GoDCmofSWE5FlwayZd-VqRABL831W7mRqSZQQU"
 mod=1072611320979017748
 ip="162.33.28.101"
 log=1072453600032342036
+api_user="AyuTheBoss"
+api_url="play.ayuitz.tech/api.php"
+api_token="n3B#s34D93Lj89"
